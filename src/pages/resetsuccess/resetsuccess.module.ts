7022d65afd6062075908fb39580ee299bd714e91
UMAIR\Umair <umair@absolutebyte.co.uk>
2017-09-21 15:23:22 +0500

    password reset successfull
    page created (page 06)

---
+++ src/pages/resetsuccess/resetsuccess.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ResetsuccessPage } from './resetsuccess';
+
+@NgModule({
+  declarations: [
+    ResetsuccessPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ResetsuccessPage),
+  ],
+})
+export class ResetsuccessPageModule {}
---
+++ src/pages/resetsuccess/resetsuccess.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ResetsuccessPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-resetsuccess',
+  templateUrl: 'resetsuccess.html',
+})
+export class ResetsuccessPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ResetsuccessPage');
+  }
+
+}
