375fd2b626b19f964fe44c3b74c11769b5118ed1
UMAIR\Umair <umair@absolutebyte.co.uk>
2017-09-21 15:38:36 +0500

    data submit page
    page created (page 27)

---
+++ src/pages/datasubmit/datasubmit.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { DatasubmitPage } from './datasubmit';
+
+@NgModule({
+  declarations: [
+    DatasubmitPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(DatasubmitPage),
+  ],
+})
+export class DatasubmitPageModule {}
